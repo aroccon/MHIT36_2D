33b5558d3789ec9474ace614f7e9d52bf1ecdc8f
aroccon <alessio.roccon@uniud.it>
2026-03-19 18:29:00 +0100

    post_ok

---
+++ post/conditional_phi_temp/main.f90
@@ -0,0 +1,86 @@
+program cond_avg_phi_T
+  implicit none
+
+  integer,  parameter :: nx = 2048, ny = 2048
+  integer,  parameter :: t_start = 4000000
+  integer,  parameter :: t_end   = 10000000
+  integer,  parameter :: t_step  = 4000
+
+  integer,  parameter :: n_tbins = 100
+  real(8),  parameter :: t_hot   =  0.5d0
+  real(8),  parameter :: t_cold  = -0.5d0
+
+  real(8), allocatable :: t_field(:,:), phi_field(:,:)
+  real(8), allocatable :: cond_phi_sum(:), cond_count(:)
+
+  integer :: i, j, snap, ibin, n_snaps
+  real(8) :: temp, bin_width, t_bin_center, phi_cond
+  character(len=128) :: fname_t, fname_phi
+
+  allocate(t_field(nx,0:ny+1))
+  allocate(phi_field(nx,0:ny+1))
+  allocate(cond_phi_sum(n_tbins), cond_count(n_tbins))
+
+  cond_phi_sum = 0.d0
+  cond_count   = 0.d0
+  bin_width    = (t_hot - t_cold) / dble(n_tbins)
+  n_snaps      = 0
+
+  do snap = t_start, t_end, t_step
+
+    write(fname_t,   '("../../src/output/t_",I8.8,".dat")')   snap
+    write(fname_phi, '("../../src/output/phi_",I8.8,".dat")') snap
+
+    open(unit=10, file=trim(fname_t), form='unformatted', access='stream', status='old')
+    read(10) t_field
+    close(10)
+
+    open(unit=11, file=trim(fname_phi), form='unformatted', access='stream', status='old')
+    read(11) phi_field
+    close(11)
+
+    do j = 1, ny
+      do i = 1, nx
+        temp = t_field(i,j)
+        ! Keep only points inside the chosen temperature range
+        if (temp >= t_cold .and. temp < t_hot) then
+          ibin = int((temp - t_cold) / bin_width) + 1
+
+          ! Safety clamp
+          ibin = max(1, min(n_tbins, ibin))
+
+          cond_phi_sum(ibin) = cond_phi_sum(ibin) + phi_field(i,j)
+          cond_count(ibin)   = cond_count(ibin)   + 1.d0
+        end if
+
+      end do
+    end do
+
+    n_snaps = n_snaps + 1
+    write(*,'("Processed snapshot ",I8)') snap
+
+  end do
+
+  open(unit=30, file='cond_avg_phi_T.dat', status='replace')
+  write(30,'(A)') '# T_bin_center   <phi|T>   count'
+
+  do ibin = 1, n_tbins
+    t_bin_center = t_cold + (dble(ibin) - 0.5d0) * bin_width
+
+    if (cond_count(ibin) > 0.d0) then
+      phi_cond = cond_phi_sum(ibin) / cond_count(ibin)
+    else
+      phi_cond = 0.d0
+    end if
+
+    write(30,'(3(ES20.10,2X))') t_bin_center, phi_cond, cond_count(ibin)
+  end do
+
+  close(30)
+
+  write(*,'("Written cond_avg_phi_T.dat (",I6," snapshots)")') n_snaps
+
+  deallocate(t_field, phi_field)
+  deallocate(cond_phi_sum, cond_count)
+
+end program cond_avg_phi_T
---
+++ post/drop_count/main.f90
@@ -0,0 +1,151 @@
+program drop_count
+  implicit none
+
+  integer, parameter :: nx = 2048, ny = 2048
+  integer, parameter :: t_start = 4000000
+  integer, parameter :: t_end   = 10000000
+  integer, parameter :: t_step  = 4000
+
+  ! 8-connected neighbour offsets (dx, dy)
+  integer, parameter :: n_nb = 8
+  integer, parameter :: dx(8) = (/ -1, 0, 1, -1, 1, -1, 0, 1 /)
+  integer, parameter :: dy(8) = (/ -1,-1,-1,  0, 0,  1, 1, 1 /)
+
+  real(8), allocatable :: phi_field(:,:), t_field(:,:)
+  logical, allocatable :: mask(:,:)
+  logical, allocatable :: visited(:,:)
+
+  ! stack for flood fill (max size = nx*ny)
+  integer, allocatable :: stack_x(:), stack_y(:)
+  integer :: stack_top
+
+  ! per-drop output
+  integer, parameter   :: max_drops = 100000
+  integer, allocatable :: drop_area(:)
+  integer, allocatable :: drop_xmin(:), drop_xmax(:)
+  integer, allocatable :: drop_ymin(:), drop_ymax(:)
+  real(8), allocatable :: drop_tsum(:)   ! accumulator for temperature
+
+  integer :: i, j, snap, n_drops, d
+  integer :: ci, cj, ni, nj
+  character(len=64) :: fname_phi, fname_t, fname_out
+
+  allocate( phi_field(nx, 0:ny+1) )
+  allocate( t_field  (nx, 0:ny+1) )
+  allocate( mask(nx, ny), visited(nx, ny) )
+  allocate( stack_x(nx*ny), stack_y(nx*ny) )
+  allocate( drop_area(max_drops) )
+  allocate( drop_xmin(max_drops), drop_xmax(max_drops) )
+  allocate( drop_ymin(max_drops), drop_ymax(max_drops) )
+  allocate( drop_tsum(max_drops) )
+
+  do snap = t_start, t_end, t_step
+
+    ! --- Read fields
+    write(fname_phi, '("phi_", I8.8, ".dat")') snap
+    write(fname_t,   '("t_",   I8.8, ".dat")') snap
+
+    open(unit=10, file='../../src/output/' // trim(fname_phi), &
+         form='unformatted', access='stream', status='old')
+    read(10) phi_field
+    close(10)
+
+    open(unit=11, file='../../src/output/' // trim(fname_t), &
+         form='unformatted', access='stream', status='old')
+    read(11) t_field
+    close(11)
+
+    ! --- Build binary mask
+    do j = 1, ny
+      do i = 1, nx
+        mask(i,j) = ( phi_field(i,j) > 0.5d0 )
+      end do
+    end do
+
+    visited = .false.
+    n_drops = 0
+
+    ! --- Scan and flood fill
+    do j = 1, ny
+      do i = 1, nx
+
+        if ( mask(i,j) .and. .not. visited(i,j) ) then
+
+          n_drops = n_drops + 1
+          if (n_drops > max_drops) then
+            write(*,*) 'ERROR: max_drops exceeded at snapshot ', snap
+            stop
+          end if
+
+          drop_area(n_drops) = 0
+          drop_tsum(n_drops) = 0.d0
+          drop_xmin(n_drops) = i;  drop_xmax(n_drops) = i
+          drop_ymin(n_drops) = j;  drop_ymax(n_drops) = j
+
+          ! push seed
+          stack_top    = 1
+          stack_x(1)   = i
+          stack_y(1)   = j
+          visited(i,j) = .true.
+
+          do while (stack_top > 0)
+
+            ! pop
+            ci = stack_x(stack_top)
+            cj = stack_y(stack_top)
+            stack_top = stack_top - 1
+
+            drop_area(n_drops) = drop_area(n_drops) + 1
+            drop_tsum(n_drops) = drop_tsum(n_drops) + t_field(ci,cj)
+
+            ! update bounding box
+            drop_xmin(n_drops) = min(drop_xmin(n_drops), ci)
+            drop_xmax(n_drops) = max(drop_xmax(n_drops), ci)
+            drop_ymin(n_drops) = min(drop_ymin(n_drops), cj)
+            drop_ymax(n_drops) = max(drop_ymax(n_drops), cj)
+
+            ! visit 8 neighbours
+            do d = 1, n_nb
+              ni = mod(ci + dx(d) - 1 + nx, nx) + 1   ! periodic x
+              nj = cj + dy(d)
+
+              if (nj < 1 .or. nj > ny) cycle           ! solid y walls
+
+              if ( mask(ni,nj) .and. .not. visited(ni,nj) ) then
+                visited(ni,nj) = .true.
+                stack_top = stack_top + 1
+                stack_x(stack_top) = ni
+                stack_y(stack_top) = nj
+              end if
+            end do
+
+          end do ! flood fill stack
+
+        end if
+
+      end do
+    end do
+
+    ! --- Write output
+    write(fname_out, '("drops_", I8.8, ".dat")') snap
+    open(unit=20, file=trim(fname_out), status='replace')
+    write(20,'(A)') '# drop_id  area(cells)  x_min  x_max  y_min  y_max  T_mean'
+    do d = 1, n_drops
+      write(20,'(I6, 2X, I8, 4(2X,I6), 2X, ES15.7)') d, &
+        drop_area(d),                          &
+        drop_xmin(d), drop_xmax(d),            &
+        drop_ymin(d), drop_ymax(d),            &
+        drop_tsum(d) / dble(drop_area(d))
+    end do
+    close(20)
+
+    write(*,'("Snapshot ",I8,": ",I6," drops found")') snap, n_drops
+
+  end do
+
+  deallocate(phi_field, t_field, mask, visited)
+  deallocate(stack_x, stack_y)
+  deallocate(drop_area, drop_xmin, drop_xmax, drop_ymin, drop_ymax)
+  deallocate(drop_tsum)
+
+end program drop_count
